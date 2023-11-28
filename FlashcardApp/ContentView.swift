f060759cebe3492268f9b54dafa928c67a06e554
Cristian Crețu <crisemcr@gmail.com>
2023-11-28 10:33:50 +0200

    a

--- FlashcardApp/ContentView.swift
+++ FlashcardApp/ContentView.swift
@@ -10,89 +10,66 @@ import SwiftData
 import LaTeXSwiftUI
 
 struct CardView: View {
-    @Binding var showResponse: Bool
-    var text: String
+    @State private var isPressed = false
+    @State private var showAnswer = false
 
     var body: some View {
-        RoundedRectangle(cornerRadius: 13, style: .continuous)
-            .fill(showResponse ? Color.blue : Color.white)
-            .frame(width: 300, height: 500)
-            .overlay(
-                ZStack(alignment: .topLeading) {
-                    LaTeX(text)
-                        .fontDesign(.serif)
-                        .font(.title)
-                        .padding(16)
-                        .foregroundColor(showResponse ? .white : .black)
-                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: showResponse ? .center : .topLeading)
-                        .animation(.spring(), value: showResponse)
+        GeometryReader { geometry in
+            ZStack(alignment: .bottomTrailing) {
+                RoundedRectangle(cornerRadius: 25, style: .continuous)
+                    .fill(showAnswer ? Color.black : Color.white)
+                    .frame(height: 600)
+                    .shadow(radius: 10)
+                    .overlay(
+                        Text(showAnswer ? "Douglas Engelbart" : "Who invented the mouse?")
+                            .font(.title)
+                            .foregroundColor(showAnswer ? .white : .black)
+                            .padding(20),
+                        alignment: .center
+                    )
 
-                    if !showResponse {
-                        VStack {
-                            Spacer()
-                            HStack {
-                                Text("Calculus")
-                                    .font(.caption)
-                                    .foregroundColor(.black.opacity(0.5))
-                                Spacer()
-                                Circle()
-                                    .fill(Color.black)
-                                    .frame(width: 10, height: 10)
-                            }
-                        }
-                        .padding(16)
-                    } else {
-                        VStack {
-                            Spacer()
-                            HStack {
-                                Button(action: {
-                                }) {
-                                    Label("Again", systemImage: "hand.thumbsdown")
-                                }
-                                .buttonStyle(.bordered)
-                                
-                                Spacer()
-                                
-                                Button(action: {
-                                }) {
-                                    Label("Good", systemImage: "hand.thumbsup")
-                                }
-                                .buttonStyle(.bordered)
-                                
-                                Spacer()
-                                
-                                Button(action: {
-                                }) {
-                                    Label("Easy", systemImage: "lightbulb")
-                                }
-                                .buttonStyle(.bordered)
-                            }
-                        }
-                        .padding(16)
-                    }
-                }
-            )
-            .onTapGesture {
-                withAnimation {
-                    showResponse.toggle()
-                }
+                Circle()
+                    .fill(showAnswer ? Color.white : Color.black)
+                    .frame(width: isPressed ? geometry.size.width * 2 : 30, height: isPressed ? geometry.size.height * 2 : 30)
+                   .offset(x: isPressed ? 0 : geometry.size.width / 2 - 15, y: isPressed ? 0 : geometry.size.height / 2 - 15)
+                    .animation(.easeInOut(duration: 0.5), value: isPressed)
+                    .frame(maxWidth: geometry.size.width, maxHeight: geometry.size.height)
+                    .clipped()
             }
-            .padding()
+        }
+        .frame(height: 600)
+        .onLongPressGesture(minimumDuration: 0.5) {_ in
+//            if isPressed {
+//                self.isPressed.toggle()
+//            } else {
+//                withAnimation(.easeInOut(duration: 0.5)) {
+//                    self.isPressed.toggle()
+//                }
+//            }
+            self.isPressed.toggle()
+        } perform: {
+            self.showAnswer.toggle()
+        }
     }
 }
 
-// Main ContentView
 struct ContentView: View {
-    @State private var showResponse1 = false
-    @State private var showResponse2 = false
-    // Add more state variables if you have more cards.
+    @State private var isPressed = false
 
     var body: some View {
         ScrollView {
             VStack {
-                CardView(showResponse: $showResponse1, text: "What is the value of  $e^{i\\pi}+1$?")
-                CardView(showResponse: $showResponse2, text: "Another question?")
-                // Add more CardView instances if needed.
+                ForEach(0..<10) { index in
+                    CardView()
+                        .padding()
+                        .onLongPressGesture(minimumDuration: 0.5, pressing: { isPressing in
+                            withAnimation(.easeInOut) {
+                                self.isPressed = isPressing
+                            }
+                        }, perform: {
+                            // Perform the action to reveal the answer here
+                        })
+                }
             }
         }
     }
@@ -100,5 +77,7 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+        .background(Color.white.ignoresSafeArea())
+        .frame(width: .infinity, height: 800)
         .modelContainer(for: Item.self, inMemory: true)
 }
